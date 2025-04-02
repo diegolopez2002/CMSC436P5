270256c8a98380637bc40dfe5163bc7f117135c9
Diego Lopez <143581969+diegolopez2002@users.noreply.github.com>
2025-04-01 22:40:12 -0400

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.project5
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+// Authors: Your Name, Partner Name
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Create our custom game view and set it as the content view.
+        val gameView = GameView(this)
+        setContentView(gameView)
+    }
+}
